7c57c5e32e0e870ea46b182dc1f841e26223978b
buihongthang <70130290+buihongthang@users.noreply.github.com>
2022-01-23 22:00:29 +0700

    commit

--- web/pages/video.js
+++ web/pages/video.js
@@ -1,17 +1,17 @@
 import React from 'react'
-// import All from '../src/component/home-video/All';
-// import Cartoon from '../src/component/home-video/Cartoon';
-// import Collection from '../src/component/home-video/Collection';
-// import Episode from '../src/component/home-video/Episode';
-// import MainHomeVideo from '../src/component/home-video/MainHomeVideo';
-// import Manga from '../src/component/home-video/Manga';
-// import News from '../src/component/home-video/News';
-// import Picked from '../src/component/home-video/Picked';
-// import Ranking from '../src/component/home-video/Ranking';
+import All from '../src/component/home-video/All';
+import Cartoon from '../src/component/home-video/Cartoon';
+import Collection from '../src/component/home-video/Collection';
+import Episode from '../src/component/home-video/Episode';
+import MainHomeVideo from '../src/component/home-video/MainHomeVideo';
+import Manga from '../src/component/home-video/Manga';
+import News from '../src/component/home-video/News';
+import Picked from '../src/component/home-video/Picked';
+import Ranking from '../src/component/home-video/Ranking';
 export default function HomeVideoPage() {
     return (
         <div className='video-page container-video' style={{ padding: "60px 0" }}>
-            {/* <div>
+            <div>
                 <MainHomeVideo />
             </div>
             <div>
@@ -37,7 +37,7 @@ export default function HomeVideoPage() {
             </div>
             <div>
                 <All />
-            </div> */}
+            </div>
         </div>
     )
 }
