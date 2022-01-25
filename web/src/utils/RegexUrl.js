0e838dbc8559fdc28e81f4495e73af1723a2f40f
minhhoangzeno <minhhoangzeno@gmail.com>
2022-01-25 15:39:47 +0700

    delete

--- web/src/utils/RegexUrl.js
+++
@@ -1,9 +0,0 @@
-import * as urlSlug from 'url-slug'
-
-export const convertUrlSlug = (value) => {
-    const url = urlSlug.convert(value, {
-        separator: '-',
-        transformer: urlSlug.LOWERCASE_TRANSFORMER
-    })
-    return url
-}
